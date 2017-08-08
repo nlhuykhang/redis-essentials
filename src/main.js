61315ecf3dc250f3f40baf0094ae5a09ae969602
Khang Nguyen-Le <nlhuykhang@gmail.com>
2017-08-08 22:30:57 +0700

    build vote system with hash

--- src/main.js
+++ src/main.js
@@ -1,30 +1,33 @@
 import redis from 'redis';
 const client = redis.createClient();
 
+function saveLink(client, id, author, title, link) {
+  client.hmset(`link:${id}`, 'author', author, 'title', title, 'link', link, 'score', 0);
+}
+
 function upVote(client, id) {
-  client.incr(`article:${id}:votes`);
+  client.hincrby(`link:${id}`, 'score', 1);
 }
 
 function downVote(client, id) {
-  client.decr(`article:${id}:votes`);
+  client.hincrby(`link:${id}`, 'score', -1);
 }
 
-function showResults(client, id) {
-  client.mget([
-    `article:${id}:headline`,
-    `article:${id}:votes`
-  ], function(err, replies) {
-    console.log(replies);
+function showDetails(client, id) {
+  client.hgetall(`link:${id}`, (err, result) => {
+    console.log(result);
   });
 }
 
-upVote(client, 1);
-upVote(client, 1);
-upVote(client, 1);
-upVote(client, 1);
-upVote(client, 1);
-downVote(client, 1);
+saveLink(client, 123, "dayvson", "Maxwell Dayvson's Github page", "https://github.com/dayvson");
+upVote(client, 123);
+upVote(client, 123);
+saveLink(client, 456, "hltbra", "Hugo Tavares's Github page", "https://github.com/hltbra");
+upVote(client, 456);
+upVote(client, 456);
+downVote(client, 456);
 
-showResults(client, 1);
+showDetails(client, 123);
+showDetails(client, 456);
 
 client.quit();
