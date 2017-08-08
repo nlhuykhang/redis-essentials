9b31ef7c166c024ea926b85bd4c7da85e1594450
Khang Nguyen-Le <nlhuykhang@gmail.com>
2017-08-08 22:12:51 +0700

    simple vote system

--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
 import redis from 'redis';
-
 const client = redis.createClient();
 
-client.set('test', 'test key value');
+function upVote(client, id) {
+  client.incr(`article:${id}:votes`);
+}
 
-client.get('test', redis.print);
+function downVote(client, id) {
+  client.decr(`article:${id}:votes`);
+}
 
+function showResults(client, id) {
+  client.mget([
+    `article:${id}:headline`,
+    `article:${id}:votes`
+  ], function(err, replies) {
+    console.log(replies);
+  });
+}
+
+upVote(client, 1);
+upVote(client, 1);
+upVote(client, 1);
+upVote(client, 1);
+upVote(client, 1);
+downVote(client, 1);
+
+showResults(client, 1);
 
 client.quit();
